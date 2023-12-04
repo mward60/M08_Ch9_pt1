78508d41483ca23794b9da58d1fd4b4e846563df
Megen <103354733+mward60@users.noreply.github.com>
2023-12-04 14:22:58 -0600

    Pre-axios
    Before axios

---
+++ element.js
@@ -0,0 +1,36 @@
+const Element = {
+  data() {
+    return {
+      input : false
+    }
+  },
+  template : `
+    <li> 
+      <span v-if="!input"> {{text}} </span>
+      <input v-else type="text" :value="text" 
+       @blur="modify($event)" ref="refInput" />
+      <button @click="remove()"> Remove </button> 
+      <button @click="input=true"> Modify </button>
+    </li>
+  `,
+  props : ["text", "index"],
+  methods : {
+    remove() {
+      // process the click on the Remove button
+      this.$emit("remove", { index : this.index });
+    },
+    modify(event) {
+      var value = event.target.value;
+      this.input = false;
+      this.$emit("modify", { index : this.index, value : 
+      value });
+    }
+  },
+  emits : ["remove", "modify"],
+  updated() {
+    // check that refInput exists, and if so, give focus to 
+    // the input field
+    if (this.$refs.refInput) this.$refs.refInput.focus();  
+  }
+}
+export default Element;
